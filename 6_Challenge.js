7238d6d93a2ed07a8a2685d72262d9be27334e9d
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-04 16:21:34 -0300

    Finish 8 Challenge

--- 6_Challenge.js
+++ 6_Challenge.js
@@ -6,7 +6,7 @@ function myFunction(a){
     return a.slice(-3)
 }
 
-console.log(myFunction([99,1,1]) )
+console.log(myFunction([99,1,1]))
 // myFunction([1,2,3,4]) Expected [2,3,4]
 // myFunction([5,4,3,2,1,0]) Expected [2,1,0]
 // myFunction([99,1,1]) Expected [99,1,1]
