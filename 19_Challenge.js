26099bb9fe1074695a59ced945e190c2eeea8df8
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-05 12:05:20 -0300

    19 Challenge finish

--- 19_Challenge.js
+++ 19_Challenge.js
@@ -4,8 +4,14 @@
 // Return the resulting array
 
 function myFunction(a,b) {
-    return new Set(a.concat(b))
+    return [...new Set(a.concat(b).sort((a,b) => a -b))]
 }
+
+// The author's solution
+
+// function myFunction(a, b) {
+//   return [...new Set([...a, ...b])].sort((x, y) => x - y);
+// }
 
 console.log(myFunction([1, 2, 3], [3, 4, 5])) //Expected [ 1, 2, 3, 4, 5 ]
 console.log(myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42])) //Expected[ -11, -10, 5, 22, 41,  42, 333]
